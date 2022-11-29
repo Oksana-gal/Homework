4a0f069885794da96ea4cf6584f0cdaea067fafe
oksana <galik.oksana@yandex.by>
2022-11-29 21:26:30 +0300

    Task 10 solution

---
+++ Task_19/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да 
+
+
+ int num, rem, sum = 0, temp;
+        
+Console.WriteLine("Найти число, является ли оно палиндромом или нет.");
+Console.Write("Введите число: ");
+num = Convert.ToInt32(Console.ReadLine());
+temp = num;
+while (num != 0)
+{
+    rem = num % 10;  
+    num = num / 10; 
+    sum = sum * 10 + rem;
+}
+Console.WriteLine("Перевернутое число равно: {0} ", sum);
+if (temp == sum) 
+{
+    Console.WriteLine("Число является палиндромом");
+}
+else
+{
+    Console.WriteLine("Число не является палиндромом");
+}
+    Console.ReadLine();
